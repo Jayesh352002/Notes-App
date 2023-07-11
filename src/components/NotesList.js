ac691cd525d151fd65d2ac5f449663cff855a1be
Food Recipes App <bafnajayesh@gmail.com>
2023-07-11 11:41:29 +0530

    first commit

---
+++ src/components/NotesList.js
@@ -0,0 +1,16 @@
+import AddNote from "./AddNote";
+import Note from "./Note";
+
+const NotesList = ({notes , handleAddNote , handleDeleteNote}) => {
+    return(
+        <div className="notes-list">
+            {notes.map((notes)=>(
+                <Note id={notes.id} text={notes.text} date={notes.date} handleDeleteNote={handleDeleteNote}/>
+            ))}
+
+            <AddNote handleAddNote={handleAddNote}/>
+        </div>
+    );
+};
+
+export default NotesList ; 
---
+++ src/components/SearchNote.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { MdSearch } from "react-icons/md";
+
+const SearchNote = ({handleSearchNote}) => {
+    return(
+        <div className="search">
+            <MdSearch className="search-icon" size='1.3em'/>
+            <input type="text" placeholder="Search" 
+            onChange={(event)=>handleSearchNote(event.target.value)}/>
+        </div>
+    );
+};
+
+export default SearchNote ;
